6a71d99479a0d43fa6da9394958b544fe7034165
alviennn <alvien.rnp@gmail.com>
2024-12-18 20:20:50 +0700

    menambahkan data class DetailBrgUiState untuk menyimpan status UI halaman detail barang, termasuk objek BarangEvent, status loading, status error, dan pesan error

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/DetailBrgViewModel.kt
@@ -2,6 +2,13 @@ package com.example.ucproomdatabase.ui.viewmodel.barang
 
 import com.example.ucproomdatabase.data.entity.Barang
 
+data class DetailBrgUiState(
+    val detailBrgUiEvent: BarangEvent = BarangEvent(),
+    val isLoading: Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+)
+
 fun Barang.toDetailUiEvent () : BarangEvent{
     return BarangEvent(
         idbarang = idbarang,
