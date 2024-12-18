f31ddae91d9157f72fd2c99249a4dbd4fd0e2d65
alviennn <alvien.rnp@gmail.com>
2024-12-18 18:51:38 +0700

    menambahkan implementasi LocalRepositoryBrg sebagai repository lokal untuk operasi data barang, menghubungkan interface RepositoryBrg dengan BarangDao untuk mengelola data melalui Room

---
+++ app/src/main/java/com/example/ucproomdatabase/repository/LocalRepositoryBrg.kt
@@ -0,0 +1,24 @@
+package com.example.ucproomdatabase.repository
+
+import com.example.ucproomdatabase.data.dao.BarangDao
+import com.example.ucproomdatabase.data.entity.Barang
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryBrg(
+    private val barangDao: BarangDao
+) : RepositoryBrg {
+    override suspend fun insertBarang(barang: Barang) {
+        barangDao.insertBarang(barang)
+    }
+    override suspend fun deleteBarang(barang: Barang) {
+        barangDao.deleteBarang(barang)
+    }
+    override suspend fun updateBarang(barang: Barang) {
+        barangDao.updateBarang(barang)
+    }
+    override fun getAllBarang(): Flow<List<Barang>> =
+        barangDao.getAllBarang()
+
+    override fun getBarang(id: Int): Flow<Barang> =
+        barangDao.getBarang(id)
+}
