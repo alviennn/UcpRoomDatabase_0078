dba2ba18341194fc631743253f9fc89591ec33f0
alviennn <alvien.rnp@gmail.com>
2024-12-18 20:09:16 +0700

    menambahkan HomeBrgViewModel untuk mengelola status UI halaman utama barang, termasuk mem-fetch data barang dari repositori, menangani loading, error, dan menampilkan daftar barang. Menggunakan StateFlow untuk mengelola state UI secara reaktif

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/HomeBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/HomeBrgViewModel.kt
@@ -1,6 +1,52 @@
 package com.example.ucproomdatabase.ui.viewmodel.barang
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdatabase.data.entity.Barang
+import com.example.ucproomdatabase.repository.RepositoryBrg
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class HomeBrgViewModel(
+    private val repositoryBrg: RepositoryBrg
+) : ViewModel() {
+
+    val homeBrgUiState: StateFlow<HomeBrgUiState> = repositoryBrg.getAllBarang()
+        .filterNotNull()
+        .map {
+            HomeBrgUiState(
+                barangList = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeBrgUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeBrgUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeBrgUiState(
+                isLoading = true
+            )
+
+        )
+}
 
 data class HomeBrgUiState(
     val barangList: List<Barang> = listOf(),
