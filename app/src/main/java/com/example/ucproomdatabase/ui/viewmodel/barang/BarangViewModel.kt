bcbb65be8147ef2a77c726254cf32ee1ebc488b2
alviennn <alvien.rnp@gmail.com>
2024-12-18 19:10:43 +0700

    menambahkan data class BarangEvent untuk menyimpan data barang yang digunakan dalam event UI, dengan atribut seperti ID, nama barang, deskripsi, harga, stok, dan nama suplier

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/BarangViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/BarangViewModel.kt
@@ -2,6 +2,17 @@ package com.example.ucproomdatabase.ui.viewmodel.barang
 
 import com.example.ucproomdatabase.data.entity.Barang
 
+
+
+data class BarangEvent(
+    val idbarang: String = "",
+    val namabarang: String = "",
+    val deskripsi: String = "",
+    val harga: Double = 0.0,
+    val stok: Int = 0,
+    val namasuplier: String = ""
+)
+
 fun BarangEvent.toBarangEntity(): Barang = Barang(
     idbarang = idbarang,
     namabarang = namabarang,
