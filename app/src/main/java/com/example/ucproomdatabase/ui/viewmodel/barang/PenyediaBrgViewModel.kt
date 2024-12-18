e36f824ae1b2ae5b6ec1c83a82634de95f5630ce
alviennn <alvien.rnp@gmail.com>
2024-12-18 20:58:51 +0700

    menambahkan objek PenyediaBrgViewModel dengan Factory untuk menyediakan inisialisasi berbagai ViewModel yang terkait dengan barang, seperti BarangViewModel, HomeBrgViewModel, DetailBrgViewModel, dan updateBrgViewModel, yang mengakses repository barang melalui dependency injection dari TokoApp

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/PenyediaBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/PenyediaBrgViewModel.kt
@@ -7,7 +7,35 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucproomdatabase.TokoApp
 
+object PenyediaBrgViewModel{
 
+    val Factory = viewModelFactory {
+        initializer {
+            BarangViewModel(
+                TokoApp().containerApp.repositoryBrg
+            )
+        }
+
+        initializer {
+            HomeBrgViewModel(
+                TokoApp().containerApp.repositoryBrg
+            )
+        }
+
+        initializer {
+            DetailBrgViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            updateBrgViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositoryBrg
+            )
+        }
+    }
+}
 
 fun CreationExtras.TokoApp() : TokoApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
