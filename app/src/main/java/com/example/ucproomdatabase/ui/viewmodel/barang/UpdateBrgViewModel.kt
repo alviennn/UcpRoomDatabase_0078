65cd90d520d69a8f4cbcc5feb25d7082ee14b444
alviennn <alvien.rnp@gmail.com>
2024-12-18 21:16:27 +0700

    menambahkan updateBrgViewModel untuk menangani logika terkait pembaruan data barang. Saat ini, kelas ViewModel ini masih kosong dan siap diisi dengan fungsi dan alur bisnis yang relevan

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/UpdateBrgViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/barang/UpdateBrgViewModel.kt
@@ -1,6 +1,24 @@
 package com.example.ucproomdatabase.ui.viewmodel.barang
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdatabase.data.entity.Barang
+import com.example.ucproomdatabase.repository.RepositoryBrg
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+
+class updateBrgViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryBrg: RepositoryBrg,
+) : ViewModel() {
+
+
+}
 
 fun Barang.toUIStateBrg() : BrgUiState = BrgUiState(
     barangEvent = this.toDetailUiEvent(),
