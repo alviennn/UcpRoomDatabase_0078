f55e6d41cf4dcf50a45b76402d5bc84c2f47266c
alviennn <alvien.rnp@gmail.com>
2024-12-19 16:27:00 +0700

    menambahkan SuplierViewModel dengan fungsi untuk mengelola status UI, validasi input, penyimpanan data suplier, dan reset pesan snackbar

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -1,6 +1,64 @@
 package com.example.ucproomdatabase.ui.viewmodel.suplier
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucproomdatabase.data.entity.Suplier
+import com.example.ucproomdatabase.repository.RepositorySpl
+import kotlinx.coroutines.launch
+
+class SuplierViewModel(private val repositorySup: RepositorySpl) : ViewModel() {
+    var uiState by mutableStateOf(SuplierUiState())
+
+    fun updateUiState(suplierEvent: SuplierEvent){
+        uiState = uiState.copy(
+            suplierEvent = suplierEvent
+        )
+    }
+
+    private fun validateFields(): Boolean{
+        val event = uiState.suplierEvent
+        val errorState = FormErrorState(
+            idsuplier = if (event.idsuplier.isNotEmpty()) "Id suplier tidak boleh kosong" else null,
+            namasuplier = if (event.namasuplier.isNotEmpty()) "Nama suplier tidak boleh kosong" else null,
+            kontak = if (event.kontak.isNotEmpty()) "Kontak suplier tidak boleh kosong" else null,
+            alamat = if (event.alamat.isNotEmpty()) "Alamat suplier tidak boleh kosong" else null
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun saveData(){
+        val currentEvent = uiState.suplierEvent
+
+        if (validateFields()){
+            viewModelScope.launch {
+                try {
+                    repositorySup.insertSuplier(currentEvent.toSuplierEntity())
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data Berhasil Disimpan",
+                        suplierEvent = SuplierEvent(),
+                        isEntryValid = FormErrorState()
+                    )
+                }catch (e: Exception){
+                    uiState = uiState.copy(
+                        snackbarMessage = "Data Gagal Disimpan"
+                    )
+                }
+            }
+        }else {
+            uiState = uiState.copy(
+                snackbarMessage = "Input tidak valid. Periksa kembali data Anda"
+            )
+        }
+    }
+    fun resetSnackbarMessage(){
+        uiState = uiState.copy(
+            snackbarMessage = null
+        )
+    }
+}
 
 data class SuplierUiState(
     val suplierEvent: SuplierEvent = SuplierEvent(),
