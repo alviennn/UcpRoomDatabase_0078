e32e2c8cc5f61614e173c00a4cab11876ca8d435
alviennn <alvien.rnp@gmail.com>
2024-12-19 16:46:22 +0700

    Menambahkan properti isUiEventEmpty dan isUiEventNotEmpty untuk memeriksa apakah detailSupUiEvent kosong atau tidak

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/DetailSplViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/DetailSplViewModel.kt
@@ -2,12 +2,20 @@ package com.example.ucproomdatabase.ui.viewmodel.suplier
 
 import com.example.ucproomdatabase.data.entity.Suplier
 
+
+
 data class DetailSupUiState(
     val detailSupUiEvent: SuplierEvent = SuplierEvent(),
     val isLoading: Boolean = false,
     val isError : Boolean = false,
     val errorMessage: String = ""
-)
+){
+    val isUiEventEmpty : Boolean
+        get() = detailSupUiEvent == SuplierEvent()
+
+    val isUiEventNotEmpty : Boolean
+        get() = detailSupUiEvent != SuplierEvent()
+}
 
 fun Suplier.toDetailUiEvent () : SuplierEvent{
     return SuplierEvent(
