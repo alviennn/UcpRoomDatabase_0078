c37453d46c0ae3e164a4ab4d8dfebab3ba2723c2
alviennn <alvien.rnp@gmail.com>
2024-12-19 15:30:29 +0700

    menambahkan data class SuplierEvent untuk merepresentasikan data pemasok dalam aplikasi

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -2,6 +2,13 @@ package com.example.ucproomdatabase.ui.viewmodel.suplier
 
 import com.example.ucproomdatabase.data.entity.Suplier
 
+data class SuplierEvent(
+    val idsuplier: String = "",
+    val namasuplier: String = "",
+    val kontak: String = "",
+    val alamat: String = ""
+)
+
 fun SuplierEvent.toSuplierEntity(): Suplier = Suplier(
     idsuplier = idsuplier,
     namasuplier = namasuplier,
