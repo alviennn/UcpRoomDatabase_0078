362189dceee9941de8a54919814cf7f8cd18ea12
alviennn <alvien.rnp@gmail.com>
2024-12-19 17:30:33 +0700

    Menambahkan factory PenyediaSplViewModel.Factory untuk menginisialisasi SuplierViewModel, HomeSplViewModel, dan DetailSplViewModel dengan dependensi repositorySpl dan SavedStateHandle

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/PenyediaSplViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/PenyediaSplViewModel.kt
@@ -1,8 +1,33 @@
 package com.example.ucproomdatabase.ui.viewmodel.suplier
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucproomdatabase.TokoApp
+
+object PenyediaSplViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            SuplierViewModel(
+                TokoApp().containerApp.repositorySpl
+            )
+        }
+        initializer {
+            HomeSplViewModel(
+                TokoApp().containerApp.repositorySpl
+            )
+        }
+        initializer {
+            DetailSplViewModel(
+                createSavedStateHandle(),
+                TokoApp().containerApp.repositorySpl
+            )
+        }
+    }
+}
 
 fun CreationExtras.TokoApp(): TokoApp =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as TokoApp)
