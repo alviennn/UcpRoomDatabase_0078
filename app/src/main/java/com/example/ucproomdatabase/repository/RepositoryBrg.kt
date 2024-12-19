95d1e743532fd2bd0096644534f3a1b6b16c7213
alviennn <alvien.rnp@gmail.com>
2024-12-19 15:27:02 +0700

    merubah tipe data id dari int menjadi string

--- app/src/main/java/com/example/ucproomdatabase/repository/RepositoryBrg.kt
+++ app/src/main/java/com/example/ucproomdatabase/repository/RepositoryBrg.kt
@@ -6,7 +6,7 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoryBrg {
     suspend fun insertBarang(barang: Barang)
     fun getAllBarang(): Flow<List<Barang>>
-    fun getBarang(id: Int): Flow<Barang>
+    fun getBarang(id: String): Flow<Barang>
     suspend fun deleteBarang(barang: Barang)
     suspend fun updateBarang(barang: Barang)
 }
