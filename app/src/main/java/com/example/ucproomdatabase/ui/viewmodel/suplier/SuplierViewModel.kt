33a815206e32924ad4e0bce6b113d0d09a47589d
alviennn <alvien.rnp@gmail.com>
2024-12-19 15:32:38 +0700

    menambahkan validasi FormErrorState untuk memastikan kelengkapan data Suplier

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/SuplierViewModel.kt
@@ -2,6 +2,17 @@ package com.example.ucproomdatabase.ui.viewmodel.suplier
 
 import com.example.ucproomdatabase.data.entity.Suplier
 
+data class FormErrorState(
+    val idsuplier: String? = null,
+    val namasuplier: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null
+){
+    fun isValid(): Boolean{
+        return idsuplier != null && namasuplier != null && kontak != null && alamat != null
+    }
+}
+
 data class SuplierEvent(
     val idsuplier: String = "",
     val namasuplier: String = "",
