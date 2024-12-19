d2caf9588b48d5921fffd7156bba1a234ae431d1
alviennn <alvien.rnp@gmail.com>
2024-12-19 16:43:01 +0700

    Menambahkan data class DetailSupUiState untuk mendukung UI detail suplier

--- app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/DetailSplViewModel.kt
+++ app/src/main/java/com/example/ucproomdatabase/ui/viewmodel/suplier/DetailSplViewModel.kt
@@ -2,6 +2,13 @@ package com.example.ucproomdatabase.ui.viewmodel.suplier
 
 import com.example.ucproomdatabase.data.entity.Suplier
 
+data class DetailSupUiState(
+    val detailSupUiEvent: SuplierEvent = SuplierEvent(),
+    val isLoading: Boolean = false,
+    val isError : Boolean = false,
+    val errorMessage: String = ""
+)
+
 fun Suplier.toDetailUiEvent () : SuplierEvent{
     return SuplierEvent(
         idsuplier = idsuplier,
